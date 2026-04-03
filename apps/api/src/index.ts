5baea13d428ea135cc2a18f54dca7639bbdc53dc
khizar Shaikh <khizarshaikh.work@gmail.com>
2026-04-03 10:39:00 +0530

    added node veresion

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,8 +4,6 @@ import { app } from "./app";
 
 const port = process.env.PORT || 4000;
 
-console.log("DATABASE_URL runtime:", process.env.DATABASE_URL);
-
 app.listen(port, () => {
   
   console.log(`API running on http://localhost:${port}`);
